1687d0a6e72d329c453894dfa3e5dedb194cf3aa
DELL <milkchocolatewithdaisy@gmail.com>
2020-01-09 16:43:41 +0400

    verson for "Git and random circles"

--- round.py
+++ round.py
@@ -1,23 +1,53 @@
 import sys
 from random import randint
-from PyQt5 import uic
+from PyQt5 import QtCore, QtGui, QtWidgets
 from PyQt5.QtWidgets import QApplication, QMainWindow
 from PyQt5.QtGui import QPainter, QColor
-from PyQt5.QtCore import Qt
 
 
-class MyWidget(QMainWindow):
+class Ui_MainWindow(object):
+    def setupUi(self, MainWindow):
+        MainWindow.setObjectName("MainWindow")
+        MainWindow.resize(800, 600)
+        self.centralwidget = QtWidgets.QWidget(MainWindow)
+        self.centralwidget.setObjectName("centralwidget")
+        self.pushButton = QtWidgets.QPushButton(self.centralwidget)
+        self.pushButton.setGeometry(QtCore.QRect(300, 260, 200, 40))
+        font = QtGui.QFont()
+        font.setPointSize(14)
+        self.pushButton.setFont(font)
+        self.pushButton.setObjectName("pushButton")
+        MainWindow.setCentralWidget(self.centralwidget)
+        self.menubar = QtWidgets.QMenuBar(MainWindow)
+        self.menubar.setGeometry(QtCore.QRect(0, 0, 800, 21))
+        self.menubar.setObjectName("menubar")
+        MainWindow.setMenuBar(self.menubar)
+        self.statusbar = QtWidgets.QStatusBar(MainWindow)
+        self.statusbar.setObjectName("statusbar")
+        MainWindow.setStatusBar(self.statusbar)
+
+        self.retranslateUi(MainWindow)
+        QtCore.QMetaObject.connectSlotsByName(MainWindow)
+
+    def retranslateUi(self, MainWindow):
+        _translate = QtCore.QCoreApplication.translate
+        MainWindow.setWindowTitle(_translate("MainWindow", "MainWindow"))
+        self.pushButton.setText(_translate("MainWindow", "Новая окружность"))
+
+
+class MyWidget(QMainWindow, Ui_MainWindow):
     def __init__(self):
         super().__init__()
-        uic.loadUi('UI.ui', self)
+        self.setupUi(self)
         self.krugs = []
         self.pushButton.clicked.connect(self.run)
 
     def run(self):
-        r = randint(1, 100)
-        x = randint(r, 800 - r)
-        y = randint(r, 600 - r)
-        self.krugs.append([x, y, r, r])
+        r = randint(1, 200)
+        x = randint(0, 800 - r)
+        y = randint(0, 600 - r)
+        color = QColor(randint(0, 255), randint(0, 255), randint(0, 255))
+        self.krugs.append([color, [x, y, r, r]])
 
     def paintEvent(self, event):
         qp = QPainter()
@@ -26,9 +56,9 @@ class MyWidget(QMainWindow):
         qp.end()
 
     def draf(self, qp):
-        qp.setBrush(QColor(255, 255, 0))
         for i in self.krugs:
-            qp.drawEllipse(*i)
+            qp.setBrush(i[0])
+            qp.drawEllipse(*i[1])
         self.update()
 
 
